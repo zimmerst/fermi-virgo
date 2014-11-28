4879500965e8d9fa50a02879b4eeec505ef77608
zimmer <zimmer@fysik.su.se>
2014-11-28 14:19:28 +0100

    minor fixes

--- tools/verifyYaml.py
+++ tools/verifyYaml.py
@@ -8,9 +8,9 @@ import yaml, sys, glob, os
 ifile = sys.argv[1]
 print 'INFO: processing path %s'%ifile
 files = glob.glob(ifile)
-print 'INFO: found files: {}'.format(files)
+print 'INFO: found %i files'len(files)
 
 for fi in files:
     fo = open(os.path.abspath(fi),'rb')
     d = yaml.load(fo)
-    print os.path.basename(fi), d.keys()
+    print os.path.basename(fi), sorted(d.keys())
